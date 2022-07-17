fea36e0e34f7fe7e51dfb282153b0d61d3ad84aa
Ali Fadhel <32642133+AliAlbabily@users.noreply.github.com>
2022-07-17 22:07:02 +0200

    Updated player form

--- client/src/components/ProfileForm.js
+++ client/src/components/ProfileForm.js
@@ -7,35 +7,50 @@ import Box from '@mui/material/Box';
 function ProfileForm() {
 
     const [selectedFile, setSelectedFile] = useState()
+    const [selectedFileName, setSelectedFileName] = useState()
+    const [selectedPlayerName, setSelectedPlayerName] = useState()
 
     const fileSelectedHandler = event => {
         setSelectedFile(URL.createObjectURL(event.target.files[0]))
     }
+    
+    const fileNameSelectedHandler = event => {
+        setSelectedFileName(event.target.files[0].name)
+    }
 
-    const fileUploadHandler = () => {
-        console.log(selectedFile)
+    const nameSelectedHandler = event => {
+        setSelectedPlayerName(event.target.value)
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        console.log(selectedFileName)
+        console.log(selectedPlayerName)
+        
     }
     
     return (
-        <FormControl sx={{ gap: 3 }}>
-            <Box 
-                component="img"
-                sx={{
-                    height: '200px',
-                    maxWidth: '200px',
-                    marginLeft: 'auto',
-                    marginRight: 'auto'
-                }}
-                style={{objectFit: 'cover', borderRadius: '5px', filter: 'drop-shadow(0 0 0.1rem rgb(0, 0, 0))'}}
-                src={selectedFile != null ? selectedFile : require('../images/picture.png')}
-            />
-            <Button variant="contained" component="label" onChange={fileSelectedHandler}>
-                Upload player image
-                <input type="file" hidden onClick={fileUploadHandler}/>
-            </Button>
-            <TextField id="outlined-basic" label="Select a name" variant="standard" />
-            <Button variant="contained" color="primary" type="submit">Submit</Button>
-        </FormControl>
+        <form onSubmit={handleSubmit}>
+            <FormControl sx={{ gap: 3 }} onSubmit={handleSubmit}>
+                <Box 
+                    component="img"
+                    sx={{
+                        height: '200px',
+                        maxWidth: '200px',
+                        marginLeft: 'auto',
+                        marginRight: 'auto'
+                    }}
+                    style={{objectFit: 'cover', borderRadius: '5px', filter: 'drop-shadow(0 0 0.1rem rgb(0, 0, 0))'}}
+                    src={selectedFile != null ? selectedFile : require('../images/picture.png')}
+                />
+                <Button variant="contained" component="label" onChange={(e) => {fileSelectedHandler(e); fileNameSelectedHandler(e)}}>
+                    Upload player image
+                    <input type="file" hidden/>
+                </Button>
+                <TextField id="outlined-basic" label="Select a name" variant="standard" onChange={nameSelectedHandler}/>
+                <Button variant="contained" color="primary" type="submit">Submit</Button>
+            </FormControl>
+        </form>
     );
 }
 

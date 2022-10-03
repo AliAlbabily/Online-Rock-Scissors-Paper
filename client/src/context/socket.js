3639d8241d5ed61973ca7df65cf2ad0fd39ffaca
Ali Fadhel <32642133+AliAlbabily@users.noreply.github.com>
2022-10-04 00:58:47 +0200

    Latest changes

--- client/src/context/socket.js
+++ client/src/context/socket.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import io from 'socket.io-client';
 
-export const socket = io.connect("http://localhost:3001/")
+const ipAddress = process.env.REACT_APP_IP
+
+export const socket = io.connect("http://" + ipAddress + ":3001/")
 export const SocketContext = React.createContext()
